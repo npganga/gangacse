f004c96eed1ac1b661bd34e71f050f23ae628409
npganga <gangangpalayam@gmail.com>
2017-07-25 16:22:42 +0530

    Add files via upload

---
+++ set114.java
@@ -0,0 +1,18 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class set114 {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter the number");
+	int n=in.nextInt();
+	in.close();
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+}
+}
