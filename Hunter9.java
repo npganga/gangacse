0b38aeba2e70005528783fa294954c9c9dfc8508
npganga <gangangpalayam@gmail.com>
2017-09-01 12:15:26 +0530

    Add files via upload

---
+++ Hunter9.java
@@ -0,0 +1,32 @@
+package gUVI;
+
+import java.util.Scanner;
+
+public class Hunter9 {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter the size of an array");
+	int n=in.nextInt();
+	System.out.println("Array elements are");
+	int[] arr=new int[n];
+	for(int i=0;i<n;i++) {
+		arr[i]=in.nextInt();
+	}
+	int b=2147483647;
+	int one=0;
+	int two=0;
+	for(int i=0;i<n-1;i++) {
+		for(int j=i+1;j<n;j++) {
+			int a=arr[i]+arr[j];
+			int d=Math.abs(a);
+			if(d<b) {
+				b=d;
+				one=arr[i];
+				two=arr[j];
+			}
+		}
+	}
+	System.out.println("The closest pair of elements near to zero is ("+one+","+two+")");
+	in.close();
+}
+}
